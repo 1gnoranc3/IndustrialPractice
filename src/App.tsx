4f84a9d70a338cda175b4efe2279101e84adf9a1
Andrey Emelianchik <112806402+1gnoranc3@users.noreply.github.com>
2025-04-28 23:10:02 +0300

    project initialised

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <h1>Hello React + TypeScript + Webpack!</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
